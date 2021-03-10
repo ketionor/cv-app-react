14a82dec37d14da8a47ca497ae551c92683f549d
Josh <joshua.sandefer@gmail.com>
2021-03-10 05:21:11 -0600

    added react-bootstrap

---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,11 @@
+import { GoDiffAdded } from 'react-icons/go'
+
+const GeneralInfo = () => {
+    return (
+        <div>
+            <GoDiffAdded />
+        </div>
+    )
+}
+
+export default GeneralInfo
